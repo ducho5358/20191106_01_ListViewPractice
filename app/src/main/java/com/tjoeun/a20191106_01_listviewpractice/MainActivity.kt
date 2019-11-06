2d3de0b15fe5a3cfcd5140e2e94841e7864e6e10
ducho <ducho5358@gmail.com>
2019-11-06 20:49:01 +0900

    getview 메소드 적용

--- app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191106_01_listviewpractice/MainActivity.kt
@@ -2,12 +2,15 @@ package com.tjoeun.a20191106_01_listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.a20191106_01_listviewpractice.adapters.NoticeAdapter
 import com.tjoeun.a20191106_01_listviewpractice.datas.NoticeData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 
     var noticeList = ArrayList<NoticeData>()
+    var noticeAdapter:NoticeAdapter? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         addNotices()
+
+        noticeAdapter = NoticeAdapter(this, noticeList)
+        noticeListView.adapter = noticeAdapter
 
 
     }
